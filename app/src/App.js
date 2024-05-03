9f80d63dcfbcc87c3cd44d37062217d261b01d74
dawidpodsiadly <dawid.podsiadly@interia.pl>
2024-05-03 17:55:34 +0200

    Change Routes to Pages

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import CreateUser from './components/CreateUser';
-import UpdateUser from './components/UpdateUser';
-import UsersPage from './pages/UsersPage'; // Importuj UsersPage
+import CreateUser from './pages/CreateUserPage';
+import UpdateUser from './pages/UpdateUserPage';
+import UsersPage from './pages/UsersPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<UsersPage />} /> {/* Trasa dla UsersPage */}
+        <Route path="/" element={<UsersPage />} />
         <Route path="/create" element={<CreateUser />} />
         <Route path="/edit/:id" element={<UpdateUser />} />
       </Routes>
