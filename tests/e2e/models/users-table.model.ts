b2d81732b0f5e6fa3c2d4c461f5728b6964ad21c
dpodsiadly <dawid.podsiadly@interia.pl>
2024-05-12 22:49:46 +0200

    Added Table model

--- tests/e2e/models/users-table.model.ts
+++ tests/e2e/models/users-table.model.ts
@@ -1,6 +1,6 @@
 import { Page, Locator } from '@playwright/test';
 
-export class UsersTable {
+export class UsersHeader {
   readonly page: Page;
 
   readonly locators: {
@@ -12,15 +12,52 @@ export class UsersTable {
     previousButton: Locator
     nextButton: Locator
     itemsSelect: Locator
-  }
-
+  };
 
   constructor(page: Page) {
     this.page = page;
 
     this.locators = {
         searchInput: this.page.locator('#search-input'),
-        addUser: this.page.locator('xd'),
+        addUser: this.page.locator('#add-user-button'),
       };
+
+    this.pagination = {
+        previousButton: this.page.locator('#pagination-prev-button'),
+        nextButton: this.page.locator('#pagination-next-button'),
+        itemsSelect: this.page.locator('#pagination-page-items'),
+    }
+  };
+};
+
+export class UsersTable {
+  readonly page: Page;
+  readonly tablePage: Locator
+
+  constructor(page: Page) {
+    this.page = page;
+    this.tablePage = this.page.locator('#users-table');
+  }
+
+  async getRowByEmail(userEmail: string): Promise<UsersTableRow> {
+    return new UsersTableRow(this.tablePage.locator('#table-user-row').filter({ hasText: userEmail}));
+  }  
+}
+
+export class UsersTableRow {
+  readonly userRowPage: Locator;
+
+  readonly actions: {
+    update: Locator
+    delete: Locator
+  }
+
+  constructor(pageLocator: Locator) {
+    this.userRowPage = pageLocator;
+
+    this.actions = {
+      update: this.userRowPage.locator('#table-user-row-update-button'),
+      delete: this.userRowPage.locator('#table-user-row-delete-button'),
+    }
   }
 }
