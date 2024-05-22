18f6d95ecd12d1828b52c000960ae197098e8b48
dpodsiadly <dawid.podsiadly@interia.pl>
2024-05-23 00:38:40 +0200

    API - Verify if acount is activated and add GET /auth

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -16,8 +16,31 @@ authController.login = async (req, res) => {
             return res.status(401).json({ message: "Invalid email or password" });
         }
 
+        if (!user.isActivated) {
+            return res.status(403).json({ message: "Account is not activated" });
+        }
+
         const token = jwt.sign({ id: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
         res.json({ token });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+authController.verifyToken = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ message: "Token not provided or is wrong" });
+        }
+
+        jwt.verify(token, 'your_jwt_secret', (err, decoded) => {
+            if (err) {
+                return res.status(401).json({ message: "Invalid token" });
+            } else {
+                return res.json({ message: "Valid token" });
+            }
+        });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
