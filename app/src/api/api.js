c3aed7994a289e303df7535c1586ccc3385203a4
dpodsiadly <dawid.podsiadly@interia.pl>
2024-05-23 00:39:51 +0200

    REACT - Added Auth Header to API Requests

--- app/src/api/api.js
+++ app/src/api/api.js
@@ -1,10 +1,24 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
-const API_URL = 'http://localhost:3006/';
+export const API_URL = 'http://localhost:3013/';
 
 const api = axios.create({
     baseURL: API_URL,
 });
+
+api.interceptors.request.use(
+    config => {
+        const token = Cookies.get('token');
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
 
 const apis = {
     insertUser: (payload) => api.post('/users', payload),
@@ -12,6 +26,7 @@ const apis = {
     updateUserById: (id, payload) => api.put(`/users/${id}`, payload),
     deleteUserById: (id) => api.delete(`/users/${id}`),
     getUserById: (id) => api.get(`/users/${id}`),
+    loginUser: (credentials) => api.post('/auth', credentials),
 };
 
 export default apis;
