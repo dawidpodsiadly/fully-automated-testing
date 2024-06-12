9fa3a17de82d94ce030f7b67746887b7569afa00
dpodsiadly <dawid.podsiadly@interia.pl>
2024-06-13 01:41:20 +0200

    Tests API - AuthService improvement

--- tests/api/src/services/auth-service.ts
+++ tests/api/src/services/auth-service.ts
@@ -13,9 +13,8 @@ export const testPassword = 'polskagurom';
 export const invalidAuthToken = { Authorization: 'Bearer invalidToken' };
 
 class AuthService {
-  async authorizeToken(email: string = TestUsers.apiTesterAdmin, password: string = testPassword) {
+  async authorizeToken(email: string = TestUsers.apiTesterAdmin, password: string = testPassword): Promise<{ Authorization: string }> {
     const response = await request(PathService.paths.auth).post('/').send(authBody(email, password));
-
     return {
       Authorization: 'Bearer ' + response.body.token,
     };
