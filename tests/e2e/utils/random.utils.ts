1d999c924d88b897e81f081d39592ee9225c6f6e
dpodsiadly <dawid.podsiadly@interia.pl>
2024-07-16 21:00:50 +0200

    Tests E2E - Updated Random Util

---
+++ tests/e2e/utils/random.utils.ts
@@ -0,0 +1,90 @@
+import { faker } from '@faker-js/faker';
+
+export const bigLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+export const smallLetters = 'abcdefghijklmnopqrstuvwxyz';
+export const letters = bigLetters + smallLetters;
+export const digits = '0123456789';
+export const alphanumeric = letters + digits;
+
+enum Positions {
+    Storekeeper = 'Storekeeper',
+    Accountant = 'Accountant',
+    IT = 'IT'
+}
+
+enum ContractTypes {
+    Employment = 'Employment',
+    Mandate = 'Mandate',
+    B2B = 'B2B'
+}
+
+export class RandomUtil {
+    randomInt = (max: number = 10000000) => Math.floor(Math.random() * max);
+    randomNameWithPrefix = () => 'e2e_' + this.randomInt().toString();
+
+    randomChar(alphabet: string = alphanumeric) {
+        return alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+    }
+
+    randomBoolean(): boolean {
+        return Math.random() < 0.5;
+    }
+
+    randomName(length = 10, alphabet = alphanumeric): string {
+        let text = '';
+        for (let i = 0; i < length; i++) {
+          text += this.randomChar(alphabet);
+        }
+        return text;
+    }
+    
+    randomStringNumber(length: number = this.randomInt()): string {
+        let number = '';
+        for (let i = 0; i < length; i++) {
+          const randomDigit = Math.floor(Math.random() * 10);
+          number += randomDigit.toString();
+        }
+        return number;
+    }
+
+    randomDate(): string {
+        return faker.date.between('1900-01-01', '2100-12-31').toISOString().split('T')[0];
+    }
+
+    randomOlderDate(startDate: string): string {
+        const startDateObj = new Date(startDate);
+        const endDate = new Date(startDateObj);
+        endDate.setFullYear(startDateObj.getFullYear() + 1);
+        return endDate.toISOString().split('T')[0];
+    }
+    
+    randomPhoneNumber() {
+        const minLength = 9;
+        const maxLength = 14
+        const phoneLength = this.randomInt(maxLength - minLength + 1) + minLength;
+
+        let phoneNumber = '';
+        for (let i = 0; i < phoneLength; i++) {
+            const randomDigit = Math.floor(Math.random() * 10);
+            phoneNumber += randomDigit.toString();
+        }
+        return phoneNumber;
+    }
+      
+    randomEmail() {
+      return `e2e_${this.randomName(10, smallLetters)}@e2e.pl`;
+    }
+    
+    randomUserContractType(): ContractTypes {
+        const contractTypes = Object.values(ContractTypes);
+        const randomIndex = this.randomInt(contractTypes.length);
+        return contractTypes[randomIndex];
+    }
+
+    randomUserPosition(): Positions {
+        const positions = Object.values(Positions);
+        const randomIndex = this.randomInt(positions.length);
+        return positions[randomIndex];
+    }
+}
+export const randomUtil = new RandomUtil;
