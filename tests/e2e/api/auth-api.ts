876af8475b6cee0ee7739e3b11d9e9754b079b40
dpodsiadly <dawid.podsiadly@interia.pl>
2024-07-16 21:01:29 +0200

    Tests E2E - Added Token generator for API Requests in E2E

---
+++ tests/e2e/api/auth-api.ts
@@ -0,0 +1,25 @@
+import { baseUrl } from '../config';
+import axios from 'axios';
+
+export enum TestUsers {
+    apiTesterAdmin = 'apitesteradmin@gmail.com',
+    apiTesterNotAdmin = 'apitesternotadmin@gmail.com',
+    apiTesterDeactivated = 'apitesterdeactivated@gmail.com',
+    apiTesterNotExisting = 'apitesternotexisting@gmail.com'
+}
+
+export const testPassword = 'polskagurom';
+
+class AuthApi {
+    private baseUrl = baseUrl + '/auth'
+
+    async createToken() {
+        try {
+            const response = await axios.post(this.baseUrl, {email: TestUsers.apiTesterAdmin, password: testPassword});
+            return response.data.token
+        } catch (error) {
+            throw new Error('Failed to Create Token, verify credentials');
+        }
+    }
+}
+export const authApi = new AuthApi();
---
+++ tests/e2e/services/api-token.service.ts
@@ -0,0 +1,12 @@
+import { authApi } from "../api/auth-api";
+
+export class ApiTokenService {
+    
+    async getAuthToken() {
+        const createTokenResponse = await authApi.createToken();
+        const bearerToken = 'Bearer ' + createTokenResponse
+        
+        return bearerToken;
+    }
+}
+export const apiTokenService = new ApiTokenService();
