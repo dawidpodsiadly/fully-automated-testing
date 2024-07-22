e561f627b13ef630a0731e4e2290eb4a3ccae077
dpodsiadly <dawid.podsiadly@interia.pl>
2024-07-22 21:09:15 +0200

    Tests E2E - Add logoutAndLogin fucntion

--- tests/e2e/utils/tests.utils.ts
+++ tests/e2e/utils/tests.utils.ts
@@ -1,6 +1,7 @@
 import { Page } from "@playwright/test";
 import { LoginPage } from "../models/login-page/login-page.model";
 import { createUserByApi } from "./users.utils";
+import { UsersTable } from "../models/users-table/users-table.model";
 
 export async function performTestInitialization(page: Page) {
     const loginPage = new LoginPage(page);
@@ -9,4 +10,12 @@ export async function performTestInitialization(page: Page) {
     await loginPage.openLoginPageAndLogin(userData.email, userData.password);
 
     return userData;
+}
+
+export async function logoutAndLogin(page: Page, email: string, password: string) {
+    const usersTable = new UsersTable(page);
+    const loginPage = new LoginPage(page);
+
+    await usersTable.logOut();
+    await loginPage.login(email, password);
 }
