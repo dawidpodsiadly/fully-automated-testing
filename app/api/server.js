8dfac9897ca2cf40be4e47b82459885a27379ca3
dawidpodsiadly <dawid.podsiadly@interia.pl>
2025-01-30 23:20:52 +0100

    Add Test Users Auto Setup when running API Server

--- app/api/server.js
+++ app/api/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const passport = require('passport');
 const morgan = require('morgan');
 const userRoutes = require('./routes/userRoutes');
+const auth = require('./auth');
 const { createTestUsers } = require('./utils/testUsersAutoSetup');
 
 const app = express();
